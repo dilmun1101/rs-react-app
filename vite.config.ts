c447425351c32fc5815354e3837f1eea5bb5e984
dilmun1101 <dilmun94@gmail.com>
2026-05-05 01:32:09 +0500

    chore: change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rs-react-app/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
