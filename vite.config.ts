8572a647460acea4ebaca95a1e3ba9a845e762c4
dilmun1101 <dilmun94@gmail.com>
2026-05-22 23:10:48 +0500

    chore: add allies

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@common': path.resolve(__dirname, './src/common'),
+      '@store': path.resolve(__dirname, './src/store'),
     },
   },
   server: {
